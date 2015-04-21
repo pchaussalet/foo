ca462cdb476b514296c50cdf303bdfeee69f14cb
Pierre Chaussalet <pchaussalet@gmail.com>
2015-04-21 08:12:51 +0000

    Add module todo

---
+++ todo.js
@@ -0,0 +1,16 @@
+/**
+ * @module //todo
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Todo
+ * @extends Montage
+ */
+exports.Todo = Montage.specialize(/** @lends Todo# */ {
+    constructor: {
+        value: function Todo() {
+            this.super();
+        }
+    }
+});
